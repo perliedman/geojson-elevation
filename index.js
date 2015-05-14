ec6ab97720497cc80c5a73c33226dfb796cf08cb
Jesse Crocker <jesse@datamongers.com>
2015-05-14 09:08:58 -0600

    add support for NODATA value instead of failing when elevation is not available

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var coordEach = require('turf-meta').coordEach;
 
 module.exports = {
-    addElevation: function(geojson, elevationProvider, cb) {
+    addElevation: function(geojson, elevationProvider, cb, nodata) {
         var waiting = 0,
             allProcessed = false;
 
@@ -10,8 +10,13 @@ module.exports = {
 
             elevationProvider.getElevation([coords[1], coords[0]], function(err, elevation) {
                 if (err) {
-                    cb(err);
-                    waiting  = -1;
+                    if(nodata != null) {
+                        coords[2] = nodata;
+                        waiting --;
+                    } else {
+                        cb(err);
+                        waiting  = -1;
+                    }
                 } else {
                     coords[2] = elevation;
                     waiting--;
